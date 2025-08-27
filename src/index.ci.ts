39c5573815c696c1803e7d7952d467998e127e09
Fernando Freitas <your-email@examplefrnd.frts@gmail.com>
2025-08-27 18:22:18 -0300

    fix: Update CI entry point with correct component imports and exports

--- src/index.ci.ts
+++ src/index.ci.ts
@@ -2,14 +2,18 @@
 // This file is used only for building the package, not for development
 
 // Sections
-export { ColorsSection } from './components/sections/ColorsSection';
-export { LayoutSection } from './components/sections/LayoutSection';
-export { InputsSection } from './components/sections/InputsSection';
+export { PaperSection } from './components/sections/PaperSection';
+export { NotesSection } from './components/sections/NotesSection';
+export { SlidesSection } from './components/sections/SlidesSection';
 
 // Panels
-export { ColorPanel } from './components/panels/ColorPanel';
-export { LayoutPanel } from './components/panels/LayoutPanel';
-export { InputsPanel } from './components/panels/InputsPanel';
+export { PaperSizePanel } from './components/panels/PaperSizePanel';
+export { MarginsPanel } from './components/panels/MarginsPanel';
+export { PaddingPanel } from './components/panels/PaddingPanel';
+export { SizeAndPositionPanel } from './components/panels/SizeAndPositionPanel';
+export { SizeFields } from './components/panels/SizeFields';
+export { PositionFields } from './components/panels/PositionFields';
+export { ResponsiveColorPicker } from './components/panels/ResponsiveColorPicker';
 
 // Compositions
 export { ColorInput } from './components/compositions/ColorInput';
@@ -17,6 +21,9 @@ export { ColorHexInput } from './components/compositions/ColorHexInput';
 export { ColorSelector } from './components/compositions/ColorSelector';
 export { RGBHSLColorSlidersInput } from './components/compositions/RGBHSLColorSlidersInput';
 export { DimensionInput } from './components/compositions/DimensionInput';
+export { LabeledColorHexInput } from './components/compositions/LabeledColorHexInput';
+export { LabeledColorPicker } from './components/compositions/LabeledColorPicker';
+export { MultipleSlidersInput } from './components/compositions/MultipleSlidersInput';
 
 // Components
 export { AspectRatioSelector } from './components/components/AspectRatioSelector';
@@ -27,22 +34,74 @@ export { PositionSelector } from './components/components/PositionSelector';
 
 // Primitives
 export { NumericInput } from './components/primitives/NumericInput';
-export { SizeFields } from './components/primitives/SizeFields';
-export { UnitSelector } from './components/primitives/UnitSelector';
+export { HexInput } from './components/primitives/HexInput';
+export { SliderInput } from './components/primitives/SliderInput';
+export { ColorSliderInput } from './components/primitives/ColorSliderInput';
+export { LockAspectRatio } from './components/primitives/LockAspectRatio';
+export { UniversalSelector } from './components/primitives/UniversalSelector';
 
 // Inputs
-export { RGBHSL } from './components/inputs/RGBHSL';
+export { RecentColors } from './components/inputs/RecentColors';
+export { ColorInput as RGBHSLColorInput } from './components/inputs/RGBHSL';
 
 // Layout
-export { FlexContainer } from './components/layout/FlexContainer';
-export { GridContainer } from './components/layout/GridContainer';
+export { FormRow } from './components/layout/FormRow';
 
 // Error
 export { ErrorBoundary } from './components/error/ErrorBoundary';
+export { ErrorDisplay, ValidationError, WarningMessage, InfoMessage, SuccessMessage } from './components/error/ErrorDisplay';
 
 // Legacy
 export { HorizontalColorPicker } from './components/legacy/HorizontalColorPicker';
+export { FluentColorPicker } from './components/legacy/FluentColorPicker';
+export { LegacyColorPicker } from './components/legacy/LegacyColorPicker';
 
 // Templates
-export { BasicTemplate } from './components/templates/BasicTemplate';
-export { AdvancedTemplate } from './components/templates/AdvancedTemplate';
+export { ComponentTemplate } from './components/templates/ComponentTemplate';
+export { ErrorHandlingTemplate } from './components/templates/ErrorHandlingTemplate';
+
+// Hooks and Utilities
+export { useInputValidation } from './hooks/useInputValidation';
+export { useComponentSize } from './hooks/useComponentSize';
+export { useColorManager } from './hooks/useColorManager';
+export { useFormValidation } from './hooks/useFormValidation';
+export { useLocalStorage } from './hooks/useLocalStorage';
+export { useDebounce } from './hooks/useDebounce';
+export { usePrevious } from './hooks/usePrevious';
+export { useToggle } from './hooks/useToggle';
+export { useWindowSize } from './hooks/useWindowSize';
+export { useClickOutside } from './hooks/useClickOutside';
+export { useHover } from './hooks/useHover';
+export { useAsync } from './hooks/useAsync';
+export { useThrottle } from './hooks/useThrottle';
+export { useKeyPress } from './hooks/useKeyPress';
+export { useIntersectionObserver } from './hooks/useIntersectionObserver';
+export { useMediaQuery } from './hooks/useMediaQuery';
+export { useScrollPosition } from './hooks/useScrollPosition';
+export { useOnlineStatus } from './hooks/useOnlineStatus';
+export { useCopyToClipboard } from './hooks/useCopyToClipboard';
+export { useGeolocation } from './hooks/useGeolocation';
+export { useNetworkStatus } from './hooks/useNetworkStatus';
+export { useFocus } from './hooks/useFocus';
+export { useEventListener } from './hooks/useEventListener';
+export { useInterval } from './hooks/useInterval';
+export { useTimeout } from './hooks/useTimeout';
+export { useUpdateEffect } from './hooks/useUpdateEffect';
+export { useWhyDidYouUpdate } from './hooks/useWhyDidYouUpdate';
+export { useForceUpdate } from './hooks/useForceUpdate';
+export { useIsMounted } from './hooks/useIsMounted';
+export { useUnmountEffect } from './hooks/useUnmountEffect';
+export { useMountEffect } from './hooks/useMountEffect';
+export { useDeepCompareEffect } from './hooks/useDeepCompareEffect';
+export { useShallowCompareEffect } from './hooks/useShallowCompareEffect';
+export { useFirstMountState } from './hooks/useFirstMountState';
+export { useIsFirstRender } from './hooks/useIsFirstRender';
+export { useIsLastRender } from './hooks/useIsLastRender';
+export { useIsMountedRef } from './hooks/useIsMountedRef';
+
+// Form Layout
+export { FormLayoutProvider, useFormLayout } from './styles/FormLayoutContext';
+
+// Layout Tokens and Styles
+export * from './styles/layoutTokens';
+export { useCommonStyles } from './styles/commonStyles';
